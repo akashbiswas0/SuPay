84a96f99980579f6daa7961f8568c95a58350e44
akashbiswas0 <akash8860biswas@gmail.com>
2025-05-19 20:19:41 +0530

    bug fix & Main Page

---
+++ Interface/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/ui/navbar'
+
+const Main = () => {
+  return (
+    <div>
+        <div>
+            <Navbar />
+        </div>
+    </div>
+  )
+}
+
+export default Main
